8f397461a0523c9ae286a016345faac176409408
Haris <haris_aksalic@hotmail.com>
2022-01-23 19:43:41 +0100

    Part two of the Second Day task

--- SecondDay/part-two.js
+++ SecondDay/part-two.js
@@ -1,2 +1,27 @@
 const { input } = require('./input');
 
+const measurments = {
+    horizontal: 0,
+    depth: 0,
+    aim: 0
+}
+
+
+
+input.forEach((item) => {
+    if(item.movement === "forward"){
+        measurments.horizontal += item.amount
+        measurments.depth += measurments.aim * item.amount
+    } else if (item.movement === "down"){
+        measurments.aim += item.amount
+    } else if (item.movement === "up"){
+        measurments.aim -= item.amount
+        
+    }
+    multiply = measurments.horizontal * measurments.depth
+})  
+
+console.log("horizontal: " + measurments.horizontal);
+console.log("depth: " + measurments.depth);
+console.log("aim: " + measurments.aim);
+console.log(multiply);
