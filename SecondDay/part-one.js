3e69bcb10f92cee3d8a4c4dd8c220fb5ea5673d0
Haris <haris_aksalic@hotmail.com>
2022-01-23 18:14:20 +0100

    Changes to Part One

--- SecondDay/part-one.js
+++ SecondDay/part-one.js
@@ -5,17 +5,17 @@ const measurments = {
     horizontal: 0,
     depth: 0
 }
-// let sumDepth = depth.reduce((partial_sum, a) => partial_sum + a, 0)
-// console.log(sumDepth);
+
 input.forEach((item) => {
     if(item.movement === "forward"){
-        measurments.horizontal = measurments.horizontal + item.amount
+        measurments.horizontal += item.amount
         // console.log(measurments.horizontal);   
     } else if (item.movement === "down"){
-        measurments.depth = measurments.depth + item.amount
+        measurments.depth += item.amount
+        
         // console.log(measurments.depth);
     } else if (item.movement === "up"){
-        measurments.depth = measurments.depth - item.amount
+        measurments.depth -= item.amount
         // console.log(measurments.depth);
     }
     multiply = measurments.horizontal * measurments.depth
