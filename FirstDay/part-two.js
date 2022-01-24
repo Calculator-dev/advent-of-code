87aeb842adabe38667ab5bc1b41140e3a292cb5a
Haris <haris_aksalic@hotmail.com>
2022-01-24 08:31:13 +0100

    Part two of the First day finished

---
+++ FirstDay/part-two.js
@@ -0,0 +1,15 @@
+const { input } = require('./input');
+
+let count = 0;
+
+for (let i = 0; i < input.length - 3; i++){
+    let sum = input[i] + input[i + 1] + input[i + 2]
+    let sum2 = input[i + 1] + input[i + 2] + input[i + 3]
+    // console.log(sum);
+    // console.log(sum2);    
+    if(sum2 > sum){
+        count++
+    }
+}
+
+console.log(count);
