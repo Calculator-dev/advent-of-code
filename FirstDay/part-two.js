b4372eaae114d3206e3aa0d8e1e9995498cc76d5
Haris <haris_aksalic@hotmail.com>
2022-01-25 06:47:17 +0100

    All comments removed

--- FirstDay/part-two.js
+++ FirstDay/part-two.js
@@ -3,10 +3,10 @@ const { input } = require('./input')
 let count = 0
 
 for(let i = 0; i < input.length - 3; i++) {
+
     let sum = input[i] + input[i + 1] + input[i + 2]
     let sum2 = input[i + 1] + input[i + 2] + input[i + 3]
-    // console.log(sum);
-    // console.log(sum2);    
+
     if (sum2 > sum) {
         count++
     }
